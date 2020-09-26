8de73549fb58db11d5735ec2c75be42a0d175bf5
Matías Troncoso <mat.informatica.ubb@gmail.com>
2020-09-26 03:24:23 -0300

    orden de compra v1

--- src/App.js
+++ src/App.js
@@ -22,11 +22,17 @@ const App = () => {
       observaciones: "Observaciones",
       telefono: "987654321",
     },
+    modulosEvento: {
+      cantidad: 1,
+      detalle:
+        "Proin ligula urna ac ante ipsum primis in faucibus.",
+      unidad: 5,
+      total: 5,
+    },
+    formaPago: {},
   };
 
-  const { evento, proveedor } = ContextOrdenCompra;
-
-  var doc = new jsPDF();
+  const { evento, proveedor, modulosEvento } = ContextOrdenCompra;
 
   const ordenCompra = () => {
     var doc = new jsPDF();
@@ -49,21 +55,6 @@ const App = () => {
     // Filas
     doc.setLineWidth(1.5);
     doc.setDrawColor(1, 42, 65);
-    doc.line(25, 45, 185, 45); // fila 0
-    doc.setLineWidth(0.45);
-    doc.setDrawColor(0, 0, 0);
-    doc.line(25, 51, 185, 51); // fila 1
-    doc.line(25, 57, 185, 57); // fila 2
-    doc.line(25, 63, 185, 63); // fila 3
-    doc.line(25, 69, 185, 69); // fila 4
-
-    // Columnas
-    doc.line(25, 45, 25, 69); // columna 0
-    doc.line(50, 45, 50, 69); // columna 1
-    doc.line(25, 45, 25, 69); // columna 2
-    doc.line(130, 51, 130, 69); // columna 3
-    doc.line(150, 51, 150, 69); // columna 4
-    doc.line(185, 45, 185, 69); // columna 5
 
     // Texto columna 1
     doc.text("Señor(es)", 27, 49); // texto columna 1 fila 1
@@ -98,17 +89,6 @@ const App = () => {
     doc.setDrawColor(1, 42, 65);
     doc.line(25, 81, 185, 81); // fila 0
     doc.setLineWidth(0.45);
-    doc.setDrawColor(0, 0, 0);
-    doc.line(25, 87, 185, 87); // fila 1
-    doc.line(25, 93, 185, 93); // fila 2
-    doc.line(25, 99, 185, 99); // fila 3
-
-    // Columnas
-    doc.line(25, 81, 25, 99); // columna 0
-    doc.line(50, 81, 50, 99); // columna 1
-    doc.line(130, 81, 130, 99); // columna 2
-    doc.line(150, 81, 150, 99); // columna 3
-    doc.line(185, 81, 185, 99); // columna 4
 
     // Texto columna 1
     doc.text("Razón Social", 27, 85); // texto columna 1 fila 1
@@ -143,38 +123,136 @@ const App = () => {
     doc.setLineWidth(0.45);
     doc.setDrawColor(0, 0, 0);
     doc.line(25, 117, 185, 117); // fila 1
-    doc.line(25, 123, 185, 123); // fila 2
-    doc.line(25, 129, 185, 129); // fila 2
-    doc.line(25, 135, 185, 135); // fila 2
-    doc.line(25, 141, 185, 141); // fila 2
-    doc.line(25, 147, 185, 147); // fila 2
-    doc.line(25, 153, 185, 153); // fila 2
-    doc.line(25, 159, 185, 159); // fila 2
-    doc.line(25, 165, 185, 165); // fila 2
-    doc.line(25, 171, 185, 171); // fila 2
-    doc.line(25, 177, 185, 177); // fila 2
-    doc.line(25, 183, 185, 183); // fila 2
-    doc.line(25, 189, 185, 189); // fila 2
-    doc.line(25, 195, 185, 195); // fila 2
-    doc.line(25, 201, 185, 201); // fila 2
-    doc.line(25, 207, 185, 207); // fila 2
-
-    doc.line(25, 207, 185, 207); // fila 2
-    doc.line(25, 207, 185, 207); // fila 2
-    doc.line(25, 207, 185, 207); // fila 2
-
-    // Columnas
-    doc.line(25, 117, 25, 207); // columna 0
-    doc.line(50, 117, 50, 207); // columna 1
-    doc.line(130, 117, 130, 207); // columna 2
-    doc.line(155, 117, 155, 207); // columna 3
-    doc.line(185, 117, 185, 207); // columna 4
 
     // Texto columna 1
     doc.text("CANTIDAD", 27, 121); // texto columna 1 fila 1
     doc.text("DETALLE", 52, 121); // texto columna 2 fila 2
-    doc.text("UNIDAD", 132, 121); // texto columna 3 fila 2
-    doc.text("TOTAL", 157, 121); // texto columna 4 fila 2
+    doc.text("UNIDAD", 152, 121); // texto columna 3 fila 2
+    doc.text("TOTAL", 170, 121); // texto columna 4 fila 2
+
+    doc.line(25, 123, 185, 123); // fila 1
+    var splitText = doc.splitTextToSize(modulosEvento.detalle, 116);
+    // var pageHeight = doc.internal.pageSize.height;
+    doc.setFontSize(11);
+    var y = 127;
+    var b = 127;
+    for (var a = 0; a <= 5; a++) {
+      doc.setFont("Helvetica", "Normal");
+      doc.setFontSize(9);
+      doc.text(modulosEvento.cantidad.toString(), 29, y ); // texto columna 1 fila 1
+      doc.text(modulosEvento.unidad.toString(), 154, y); // texto columna 1 fila 1
+      doc.text(modulosEvento.total.toString(), 172, y ); // texto columna 1 fila 1
+      for (var i = 0; i < splitText.length; i++) {
+        if (y > 275) {
+          y = 127;
+          doc.addPage();
+        }
+        doc.text(52, y, splitText[i]);
+
+        y = y + 5;
+      }
+      b = b + 5;
+      doc.line(25, y - 3, 185, y - 3); // fila 3
+    }
+
+    doc.line(25, 192, 185, 192);
+    doc.setFont("Helvetica", "Bold");
+    doc.text("FORMA DE PAGO", 32, 200);
+    doc.setFont("Helvetica", "Normal");
+    
+    var splitTextFP = doc.splitTextToSize(modulosEvento.detalle, 80);
+    doc.setFontSize(11);
+    var y2 = 197;
+    var objText = {
+      align: "justify",
+    };
+    doc.setFont("Helvetica", "Normal");
+    doc.setFontSize(9);
+    for (var i2 = 0; i2 < splitTextFP.length; i2++) {
+      if (y2 > 275) {
+        y2 = 197;
+        doc.addPage();
+      }
+      doc.text(66, y2, splitTextFP[i2], objText);
+      y2 = y2 + 5;
+    }
+
+    doc.line(64, 192, 64, 207); // columna
+    doc.line(25, 207, 185, 207); // fila
+    doc.setFont("Helvetica", "Bold");
+    doc.text("NETO", 152, 196);
+    doc.text("IVA", 152, 200);
+    doc.text("TOTAL", 152, 205);
+
+    doc.setFont("Helvetica", "Normal");
+    doc.setFontSize(9);
+
+    doc.text("99.999.999", 170, 195); // VALOR NETO
+    doc.text("99.999.999", 170, 200); // VALOR IVA
+    doc.text("99.999.999", 170, 206); // VALOR TOTAL
+
+    doc.line(150, 197, 185, 197); // fila
+    doc.line(150, 202, 185, 202); // fila
+
+    doc.line(25, 207, 185, 207); // fila
+    doc.line(25, 207, 185, 207); // fila
+    doc.line(25, 207, 185, 207); // fila
+
+    // Columnas
+    doc.line(25, 117, 25, 207); // columna 0
+    doc.line(50, 117, 50, 192); // columna 1
+    doc.line(150, 117, 150, 207); // columna 2
+    doc.line(168, 117, 168, 207); // columna 3
+    doc.line(185, 117, 185, 207); // columna 4
+
+    doc.setFont("Helvetica", "Bold");
+    doc.text("EVENTO:", 25, 220); 
+    doc.text("ENCARGADO:", 25, 225);
+    doc.text("FECHA ENVIO OC:", 25, 230); 
+    doc.text("FECHA REALIZACION:", 25, 235); 
+    doc.text("LUGAR REALIZACION:", 25, 240);  
+
+    doc.text("OBSERVACIONES:", 25, 250); // 
+
+    doc.setFont("Helvetica", "Normal");
+    doc.text("Matías Troncoso.", 65, 220); 
+    doc.text("12 de septiembre de 2020", 65, 225);
+    doc.text("12 de septiemre de 2020", 65, 230); 
+    doc.text("12 de septiemre de 2020", 65, 235); 
+    doc.text("Concepción", 65, 240);  
+
+
+    var splitText3 = doc.splitTextToSize(modulosEvento.detalle, 80);
+    doc.setFontSize(11);
+    var y3 = 250;
+    var objText2 = {
+      align: "justify",
+    };
+    doc.setFont("Helvetica", "Normal");
+    doc.setFontSize(9);
+    for (var i3 = 0; i3 < splitText3.length; i3++) {
+      if (y3 > 275) {
+        y3 = 250;
+        doc.addPage();
+      }
+      doc.text(65, y3, splitText3[i3], objText2);
+      y3 = y3 + 5;
+    }
+
+
+    doc.text("Nicolás Moncada P.", 153, 253); 
+    doc.text("Hatt SpA", 161, 256.6);  
+
+    doc.setLineWidth(5.0);
+    doc.setDrawColor(1, 42, 65);
+    doc.line(25, 270, 185, 270); // fila 0
+    doc.setFont("Helvetica", "bold");
+
+    doc.setTextColor(255, 255, 255);
+    doc.text("www.hatt.cl", 100, 271.5); 
+    doc.setLineWidth(0.2);
+    doc.setDrawColor(255, 255, 255);
+    doc.line(100, 271.8, 118, 271.8); // fila 0
 
     // Fin Tabla datos modulos
 
